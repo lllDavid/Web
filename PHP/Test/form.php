e47630e76938dc7847bad1963d8b22038eb3b7ef
lllDavid <imperfectrising5527@gmail.com>
2024-04-08 11:08:24 +0200

    sync

--- PHP/Test/form.php
+++ PHP/Test/form.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Form/title>
+    <title>Login</title>
 </head>
 <body>
 
-<h2>Please Login</h2>
+<h2>Login</h2>
 
 <form method="post" action="#">
-    <label for="password">Password:</label><br>
-    <input type="password" id="password"><br><br>
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br><br>
     
     <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email"><br><br>
+    <input type="text" id="email" name="email"><br><br>
     
     <input type="submit" value="Submit">
 </form>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $passsword = $_POST['password'];
+    $username = $_POST['username'];
     $email = $_POST['email'];
     
-    echo "Password: " . $password. "<br>";
-    echo "Email: " . $email;
+    echo "<br>";
+    echo "Your Username: " . $username . "<br>";
+    echo "Your Email: " . $email;
 }
 ?>
 
