9d354d831ceddb5d6d79fc0708a6f2904d8f5f49
lllDavid <imperfectrising5527@gmail.com>
2024-06-27 11:15:33 +0200

    sync

--- JavaScript/Prüfungen/test2.js
+++ JavaScript/Prüfungen/test2.js
@@ -9,8 +9,7 @@ function showTable() {
     for (var i = 0, row; row = table.rows[i]; i++) {
         for (var j = 0, col; col = row.cells[j]; j++) {
             liste.push(col.innerHTML + "<br>")
-            
-        }
+             }
     }
     output.innerHTML = liste
 }
